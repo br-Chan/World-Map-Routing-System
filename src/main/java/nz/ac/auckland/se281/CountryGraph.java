9a7d4494a373fca1e4828f446aaada71eaa4b1f7
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-05-21 14:34:34 +1200

    Enable loading of countries into graph, and implement showInfoCountry method in MapEngine class

--- src/main/java/nz/ac/auckland/se281/CountryGraph.java
+++ src/main/java/nz/ac/auckland/se281/CountryGraph.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,27 @@ public class CountryGraph {
 
   public CountryGraph() {
     this.adjNodes = new HashMap<>();
+
+  }
+
+  public Country getCountryByName(String name) {
+    for (Country country : adjNodes.keySet()) {
+      if (country.getName().equals(name)) {
+        return country;
+      }
+    }
+    
+    return null;
+  }
+
+  public void addNode(Country country) {
+    adjNodes.putIfAbsent(country, new ArrayList<>());
+
+  }
+
+  public void removeNode(Country country) {
+    adjNodes.remove(country);
+
   }
 
 }
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  CountryGraph graph;
 
   public MapEngine() {
     // add other code here if you want
+    graph = new CountryGraph();
     loadMap(); // keep this mehtod invocation
   }
 
@@ -15,11 +17,34 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+    
+    String[] countryField;
+    String[] adjacencyField;
+    for (int c = 0; c < countries.size(); c++) {
+      countryField = countries.get(c).split(",");
+
+      String countryName = countryField[0];
+      String countryContinent = countryField[1];
+      int countryFee = Integer.valueOf(countryField[2]);
+      graph.addNode(new Country(countryName, countryContinent, countryFee));
+
+      adjacencyField = adjacencies.get(c).split(",");
+    }
+
   }
 
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
-    // add code here
+    MessageCli.INSERT_COUNTRY.printMessage();
+
+    String input = Utils.scanner.nextLine();
+    Country country = graph.getCountryByName(input);
+
+    MessageCli.COUNTRY_INFO.printMessage(
+        country.getName(),
+        country.getContinent(),
+        String.valueOf(country.getTaxFee())
+    );
   }
 
   /** this method is invoked when the user run the command route. */
